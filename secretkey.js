f02559ef74328dade010939e55d243c2df123935
jebitok-dev <sharonkosgei4@gmail.com>
2021-12-21 18:49:09 +0300

    Add Changes to SecretKey File

--- secretkey.js
+++ secretkey.js
@@ -8,3 +8,28 @@ const one_time_code = crypto.randomBytes(16);
 const cipher = crypto.createCipheriv(algorithm, shared_key, one_time_code);
 let cipher_text;
 const decipher = crypto.createDecipheriv(algorithm, shared_key, one_time_code);
+
+function Decrypting() {
+    decipher.on('readable', () => {
+        let _plain_text = decipher.read();
+        if(_plain_text) {
+            console.log(_plain_text.toString('utf8'))
+        };
+    });
+
+    decipher.write(cipher_text, 'hex');
+    decipher.end();
+}
+
+function Encrypting() {
+    const marks_message = "How are you doing?";
+    cipher.on('readable', () => {
+        let _cipher_text = cipher.read();
+        if(_cipher_text) {
+            cipher_text = _cipher_text.toString('hex');
+        };
+    });
+
+    cipher.write(marks_message);
+    cipher.end();
+}
